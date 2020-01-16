74cf370d8a7aa3277ca1dd2cc08b0d5ef54f535a
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-01-16 17:56:08 +0100

    Add /app route and GameLog component

---
+++ src/views/GameLog.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Currently playing</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Single-line item</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title>Three-line item</v-list-item-title>
+              <v-list-item-subtitle>
+                Secondary line text Lorem ipsum dolor sit amet,
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                consectetur adipiscing elit.
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Backlog</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Single-line item</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title>Three-line item</v-list-item-title>
+              <v-list-item-subtitle>
+                Secondary line text Lorem ipsum dolor sit amet,
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                consectetur adipiscing elit.
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Wishlist</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Single-line item</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title>Three-line item</v-list-item-title>
+              <v-list-item-subtitle>
+                Secondary line text Lorem ipsum dolor sit amet,
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                consectetur adipiscing elit.
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Completed</v-toolbar-title>
+          </v-toolbar>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Single-line item</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title>Three-line item</v-list-item-title>
+              <v-list-item-subtitle>
+                Secondary line text Lorem ipsum dolor sit amet,
+              </v-list-item-subtitle>
+              <v-list-item-subtitle>
+                consectetur adipiscing elit.
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
