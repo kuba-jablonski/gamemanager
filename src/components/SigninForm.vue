04eac4724ae588c6007c702f68f7c5bad342a874
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-01-17 18:35:58 +0100

    add forms to homepage and improve basic design

---
+++ src/components/SigninForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-text class="font-weight-bold ">
+      <v-btn @click="$emit('onFormChange')" small text>
+        create a new account
+      </v-btn>
+    </v-card-text>
+    <v-card-title class="mb-6 d-flex justify-center"
+      ><h2>Sign In</h2></v-card-title
+    >
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" required outlined></v-text-field>
+        <v-text-field label="Password" required outlined></v-text-field>
+        <div class="d-flex justify-center"></div>
+        <v-btn depressed large>
+          Submit
+        </v-btn>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,45 @@
 <template>
-  <div class="home"></div>
+  <v-container>
+    <v-row class="mt-10">
+      <v-col :cols="6">
+        <h1 class="mb-5 display-2 font-weight-bold font-italic">
+          Manage Your<br />
+          Games With Ease
+        </h1>
+        <p class="mb-5 subtitle font-weight-light">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          quaerat odio molestiae, nihil et debitis doloribus cum mollitia quod
+          minima officia? Provident rerum iste cum pariatur error obcaecati odio
+          odit, dolores quasi animi repellat illum totam ducimus maiores
+          repellendus quibusdam nobis nam perspiciatis sequi voluptates impedit
+          fuga. Voluptates, veritatis enim!
+        </p>
+        <v-btn depressed large>Learn more</v-btn>
+      </v-col>
+      <v-col :cols="5" offset="1">
+        <v-fade-transition mode="out-in">
+          <signin-form v-if="signin" @onFormChange="signin = false" />
+          <signup-form v-else @onFormChange="signin = true" />
+        </v-fade-transition>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import SigninForm from "@/components/SigninForm";
+import SignupForm from "@/components/SignupForm";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    SigninForm,
+    SignupForm
+  },
+  data() {
+    return {
+      signin: true
+    };
+  }
 };
 </script>
