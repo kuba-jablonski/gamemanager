dd596297605360b49505574b10d396bb2f071112
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-03-17 22:30:06 +0100

    add logo image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,104 @@
 <template>
-  <div>
-    <v-content>
-      <v-container>
-        <v-row class="mt-10">
-          <v-col :cols="6">
-            <h1 class="mb-5 display-2 font-weight-bold font-italic">
-              Manage Your<br />
-              Games With Ease
-            </h1>
-            <p class="mb-5 subtitle font-weight-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              quaerat odio molestiae, nihil et debitis doloribus cum mollitia
-              quod minima officia? Provident rerum iste cum pariatur error
-              obcaecati odio odit, dolores quasi animi repellat illum totam
-              ducimus maiores repellendus quibusdam nobis nam perspiciatis sequi
-              voluptates impedit fuga. Voluptates, veritatis enim!
+  <div class="home">
+    <div class="content">
+      <div class="login white">
+        <v-container fill-height class="d-flex flex-column justify-center">
+          <div class="d-flex">
+            <v-img src="@/assets/logo.png"></v-img>
+            <h2>GameManager</h2>
+          </div>
+          <h1>Welcome Back</h1>
+          <v-form class="form mt-8" @submit.prevent="onSubmit">
+            <v-text-field
+              v-model="email"
+              label="Email"
+              required
+              dense
+              outlined
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              required
+              dense
+              outlined
+            ></v-text-field>
+            <div class="d-flex justify-center"></div>
+            <v-btn type="submit" depressed large>
+              Submit
+            </v-btn>
+          </v-form>
+        </v-container>
+      </div>
+      <div class="bullshit blue lighten-5">
+        <v-container>
+          <div class="d-flex flex-column align-end">
+            <h2 class="title mb-3">About GameManager</h2>
+            <p class="d-flex flex-column align-end mb-5 font-weight-light">
+              <span>
+                This is a fullstack learning project.
+              </span>
+              <span>Created by Jakub Jabłoński.</span>
             </p>
-            <v-btn depressed large>Learn more</v-btn>
-          </v-col>
-          <v-col :cols="5" offset="1">
-            <v-fade-transition mode="out-in">
+
+            <v-btn outlined>Learn more</v-btn>
+          </div>
+        </v-container>
+
+        <game-svg class="icon" />
+      </div>
+    </div>
+    <!-- <v-container fluid>
+      <v-row class="mt-10">
+        <v-col class="dark" :cols="6">
+          <game-svg class="icon" />
+        </v-col>
+        <v-col :cols="5" offset="1">
+          <h2 class=" headline	">Sign in to GameManager</h2>
+          <p class="font-weight-light subtitle-2">
+            Enter details below
+          </p>
+          <v-form class="mt-8" @submit.prevent="onSubmit">
+            <v-text-field
+              v-model="email"
+              label="Email"
+              required
+              dense
+              outlined
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              required
+              dense
+              outlined
+            ></v-text-field>
+            <div class="d-flex justify-center"></div>
+            <v-btn type="submit" depressed large>
+              Submit
+            </v-btn>
+          </v-form>
+          <v-fade-transition mode="out-in">
               <signin-form v-if="signin" @onFormChange="signin = false" />
               <signup-form v-else @onFormChange="signin = true" />
             </v-fade-transition>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+        </v-col>
+      </v-row>
+    </v-container> -->
   </div>
 </template>
 
 <script>
-import SigninForm from "@/components/SigninForm";
-import SignupForm from "@/components/SignupForm";
+// import SigninForm from "@/components/SigninForm";
+// import SignupForm from "@/components/SignupForm";
+import GameSvg from "@/assets/game.svg";
 
 export default {
   name: "home",
   components: {
-    SigninForm,
-    SignupForm
+    // SigninForm,
+    // SignupForm,
+    GameSvg
   },
   data() {
     return {
@@ -47,3 +107,40 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.home {
+  height: 100%;
+}
+
+.content {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 45% 55%;
+}
+
+.form {
+  width: 100%;
+  max-width: 400px;
+}
+
+.logo-svg {
+  width: 30px;
+}
+
+.login {
+  /* background-color: red; */
+}
+
+.bullshit {
+  position: relative;
+  padding-right: 30px;
+}
+
+.icon {
+  width: 75%;
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+}
+</style>
