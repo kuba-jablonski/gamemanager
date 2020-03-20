2f18b95f5a0124b623c087144e03b06ed3cfb740
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-03-20 05:16:34 +0100

    add icon to navbar

--- src/components/LayoutApp.vue
+++ src/components/LayoutApp.vue
@@ -1,30 +1,17 @@
 <template>
   <div>
     <v-app-bar app>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-img
+        alt="GameManager Logo"
+        class="shrink"
+        contain
+        src="@/assets/logo.png"
+        transition="scale-transition"
+        width="50"
+      />
       <v-spacer></v-spacer>
       <slot name="search" />
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
